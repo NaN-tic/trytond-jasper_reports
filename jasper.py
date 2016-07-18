3df8ac1920d7f8cf63fcbec6326cc25911274b39
Sergio Morillo <sergio.morillo@datalife.com.es>
2016-07-18 18:02:45 +0200

    Change SOURCES_DIR composing (grafted from aacde9fb9bcb946c304cce29144874ba7f5b5233)

--- jasper.py
+++ jasper.py
@@ -15,6 +15,7 @@ from trytond.config import config
 from trytond.pool import Pool
 from trytond.transaction import Transaction
 from trytond.cache import Cache
+from trytond.modules import MODULES_PATH
 
 from .JasperReports import JasperReport as JReport, JasperServer
 from .JasperReports import CsvRecordDataGenerator, CsvBrowseDataGenerator
@@ -240,8 +241,9 @@ class JasperReport(Report):
             'password': cls.password(),
             'subreports': subreportDataFiles,
         }
-        sources_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-            '..', os.path.dirname(action_report.report)) + os.sep
+        sources_dir = os.path.join(
+            MODULES_PATH,
+            os.path.dirname(action_report.report) + os.sep)
         parameters = {
             'STANDARD_DIR': report.standardDirectory(),
             'REPORT_LOCALE': locale,
